0a477b06109a16a3b68852da9e4b1b138a9ab9ab
Pen Yosuke Sugawara <yows1031@gmail.com>
2018-09-20 23:05:17 +0900

    no message

--- app/src/main/java/yosukesugawara/helptaxi/secondActivity.kt
+++ app/src/main/java/yosukesugawara/helptaxi/secondActivity.kt
@@ -1,5 +1,6 @@
 package yosukesugawara.helptaxi
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,7 @@ class secondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
     }
 }
