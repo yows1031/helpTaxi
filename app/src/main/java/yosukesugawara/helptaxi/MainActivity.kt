c1fbaf745931a900964243bf8545ed98ac34b28b
Pen Yosuke Sugawara <yows1031@gmail.com>
2018-09-24 16:12:40 +0900

    途中経過

--- app/src/main/java/yosukesugawara/helptaxi/MainActivity.kt
+++ app/src/main/java/yosukesugawara/helptaxi/MainActivity.kt
@@ -1,12 +1,35 @@
 package yosukesugawara.helptaxi
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    val des = findViewById<EditText>(R.id.des)
+    val det = findViewById<EditText>(R.id.det)
+    val go = findViewById<ImageButton>(R.id.go)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        go.setOnClickListener {
+            val destination = des.text.toString()
+            val detail = det.text.toString()
+
+            val goSecond = Intent(this, secondActivity::class.java)
+            intent.putExtra("desti",destination)
+            intent.putExtra("detai",detail)
+            startActivity(goSecond)
+
+        }
     }
+
 }
+
+
--- app/src/main/java/yosukesugawara/helptaxi/secondActivity.kt
+++ app/src/main/java/yosukesugawara/helptaxi/secondActivity.kt
@@ -1,15 +1,32 @@
 package yosukesugawara.helptaxi
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class secondActivity : AppCompatActivity() {
+
+    var des1 = findViewById<TextView>(R.id.des1)
+    var go = findViewById<ImageButton>(R.id.go)
+
+    var destination = intent.getStringArrayExtra("desti")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        des1.text = destination.toString()
+
+        go.setOnClickListener {
+            val thirdgo = Intent(this, thirdActivity::class.java)
+            startActivity(thirdgo)
+        }
+
+
 
     }
 }
